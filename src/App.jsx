ccad0aed21a91d07cdbacb36cac24bfb9fd92986
asdfghjkllkjhgfsa <talipovarobiya1@gmail.com>
2025-04-19 10:51:34 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Tolipov Muhammad</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum sunt quaerat deserunt dolor. Facere illo, alias voluptatum mollitia quisquam recusandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est rerum tempore recusandae repellat suscipit temporibus officiis iste a porro beatae reprehenderit nulla atque, ipsum optio tempora rem quas, labore
+      </p>
+      <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>Tailwind css</li>
+      <li>Javascript</li>
+      <li>React</li>
+      </ul>
+    </>
+  )
+}
+export default App
